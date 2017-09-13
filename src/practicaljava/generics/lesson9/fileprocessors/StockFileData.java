f150294c5364c94f62f4f5769355c7dc60af5c8d
MANISH-GIRI <manish.giri.me@gmail.com>
2017-09-12 23:46:25 -0400

    Commit fileprocessors package

---
+++ src/practicaljava/generics/lesson9/fileprocessors/StockFileData.java
@@ -0,0 +1,18 @@
+package practicaljava.generics.lesson9.fileprocessors;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class StockFileData {
+	
+	List<HashMap<String, Double>> data = new LinkedList<>();
+	
+	public void printData(){
+		System.out.println(data);
+	}
+	
+	public void addData(List<HashMap<String, Double>> dataIn){
+		data = dataIn;
+	}
+}
